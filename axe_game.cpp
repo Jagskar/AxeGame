524e70336c28c91c289e55b39ff37068385cf291
Jags <jags.saredia@cadju.com>
2022-04-01 16:48:59 +0100

    Refactoring and cleaning up code

--- axe_game.cpp
+++ axe_game.cpp
@@ -4,35 +4,47 @@
 #define BACKGROUND_COLOR  (Color) { 128, 0, 128, 255 }
 #define PLAYER_COLOR      (Color) { 189, 53, 122, 255 }
 #define OBSTACLE_COLOR    (Color) { 0, 0, 0, 255 }
+
+// FRAME RATE CONSTRAINT
 const int fps = 60;
+
+// OBSTACLE CONSTRAINTS
+const int obstacleHeight = 50;
+const int obstacleWidth = 50;
+
+// WINDOW CONSTRAINTS
+const int windowWidth = 1366;
+const int windowHeight = 768;
+
+int calculateMoveSpeed(int posY, int moveSpeed)
+{
+    if ((posY <= 0 && moveSpeed < 0) || (posY + obstacleHeight >= windowHeight && moveSpeed > 0))
+    {
+        return moveSpeed *= -1;
+    }
+    return moveSpeed;
+}
 
 int main()
 {
-    // WINDOW CONSTRAINTS
-    int width = 1366;
-    int height = 768;
-
     // CIRCLE CONSTRAINTS
-    int centerX = width / 2;
-    int centerY = height / 2;
+    int centerX = windowWidth / 2;
+    int centerY = windowHeight / 2;
     int radius = 25;
     int moveSpeed = 10;
 
-    // OBSTACLE CONSTRAINTS
-    int obstacleHeight = 50;
-    int obstacleWidth = 50;
-
-    int obstacle1X = (width * 0.25) + obstacleWidth;
+    // OBSTACLE SETUP
+    int obstacle1X = (windowWidth * 0.25) + obstacleWidth;
     int obstacle1Y = 0;
     int obstacle1MoveSpeed = 5;
     
-    int obstacle2X = (width * 0.75) - obstacleWidth;
-    int obstacle2Y = height - obstacleHeight;
+    int obstacle2X = (windowWidth * 0.75) - obstacleWidth;
+    int obstacle2Y = windowHeight - obstacleHeight;
     int obstacle2MoveSpeed = 5;
 
     const char* title = "Axe Game";
 
-    InitWindow(width, height, title);
+    InitWindow(windowWidth, windowHeight, title);
 
     SetTargetFPS(fps);
     while (!WindowShouldClose())
@@ -40,25 +52,18 @@ int main()
         BeginDrawing();
         ClearBackground(BACKGROUND_COLOR);
 
-        DrawRectangle(obstacle1X, obstacle1Y,obstacleWidth, obstacleHeight, OBSTACLE_COLOR);
+        DrawRectangle(obstacle1X, obstacle1Y, obstacleWidth, obstacleHeight, OBSTACLE_COLOR);
         DrawRectangle(obstacle2X, obstacle2Y, obstacleWidth, obstacleHeight, OBSTACLE_COLOR);
 
         obstacle1Y += obstacle1MoveSpeed;
         obstacle2Y += obstacle2MoveSpeed;
 
-        if ((obstacle1Y <= 0 && obstacle1MoveSpeed < 0) || (obstacle1Y + obstacleHeight >= height && obstacle1MoveSpeed > 0))
-        {
-            obstacle1MoveSpeed *= -1;
-        }
-
-        if ((obstacle2Y <= 0 && obstacle2MoveSpeed < 0) || (obstacle2Y + obstacleHeight >= height && obstacle2MoveSpeed > 0))
-        {
-            obstacle2MoveSpeed *= -1;
-        }
+        obstacle1MoveSpeed = calculateMoveSpeed(obstacle1Y, obstacle1MoveSpeed);
+        obstacle2MoveSpeed = calculateMoveSpeed(obstacle2Y, obstacle2MoveSpeed);
 
         DrawCircle(centerX, centerY, radius, PLAYER_COLOR);
 
-        if (IsKeyDown(KEY_D) && (centerX + radius) < width)
+        if (IsKeyDown(KEY_D) && (centerX + radius) < windowWidth)
         {
             centerX += moveSpeed;
         }
@@ -70,7 +75,7 @@ int main()
         {
             centerY -= moveSpeed;
         }
-        if (IsKeyDown(KEY_S) && (centerY + radius) < height)
+        if (IsKeyDown(KEY_S) && (centerY + radius) < windowHeight)
         {
             centerY += moveSpeed;
         }
