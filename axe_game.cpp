94baedafdf9a08442f5141d0498b195c2ed7eee0
Jags <jags.saredia@cadju.com>
2022-04-01 15:02:24 +0100

    Adding custom colors, formatting code, drawing a circle to the screen

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,9 +1,19 @@
 #include "raylib.h"
 
+#define BACKGROUND_COLOR  (Color) { 128, 0, 128, 255 }
+#define CIRCLE_COLOR      (Color) { 189, 53, 122, 255 }
+
 int main()
 {
+    // WINDOW CONSTRAINTS
     int width = 1366;
     int height = 768;
+
+    // CIRCLE CONSTRAINTS
+    int centerX = width / 2;
+    int centerY = height / 2;
+    int radius = 25;
+
     const char* title = "Axe Game";
 
     InitWindow(width, height, title);
@@ -11,7 +21,8 @@ int main()
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(BACKGROUND_COLOR);
+        DrawCircle(centerX, centerY, radius, CIRCLE_COLOR);
         EndDrawing();
     }
 }
