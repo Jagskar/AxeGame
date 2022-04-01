db2f897393d5d996a08ae91425646fdb01de7ee4
Jags <jags.saredia@cadju.com>
2022-04-01 15:36:39 +0100

    Adding movement logic

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,7 +1,9 @@
 #include "raylib.h"
 
 #define BACKGROUND_COLOR  (Color) { 128, 0, 128, 255 }
-#define CIRCLE_COLOR      (Color) { 189, 53, 122, 255 }
+#define PLAYER_COLOR      (Color) { 189, 53, 122, 255 }
+#define OBSTACLE_COLOR    (Color) { 0, 0, 0, 255 }
+const int fps = 60;
 
 int main()
 {
@@ -13,16 +15,37 @@ int main()
     int centerX = width / 2;
     int centerY = height / 2;
     int radius = 25;
+    int moveSpeed = 10;
 
     const char* title = "Axe Game";
 
     InitWindow(width, height, title);
 
+    SetTargetFPS(fps);
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BACKGROUND_COLOR);
-        DrawCircle(centerX, centerY, radius, CIRCLE_COLOR);
+
+        DrawCircle(centerX, centerY, radius, PLAYER_COLOR);
+
+        if (IsKeyDown(KEY_D) && (centerX + radius) < width)
+        {
+            centerX += moveSpeed;
+        }
+        if (IsKeyDown(KEY_A) && (centerX - radius) > 0)
+        {
+            centerX -= moveSpeed;
+        }
+        if (IsKeyDown(KEY_W) && (centerY - radius) > 0)
+        {
+            centerY -= moveSpeed;
+        }
+        if (IsKeyDown(KEY_S) && (centerY + radius) < height)
+        {
+            centerY += moveSpeed;
+        }
+
         EndDrawing();
     }
 }
